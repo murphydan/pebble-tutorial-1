5d3a95eeee0815a2ce54685d9b50644009b6a540
Dan Murphy <dan.murphy@octanner.com>
2014-10-12 14:49:37 -0600

    Formatting changes to match example

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <pebble.h>
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
@@ -23,18 +24,12 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-}
-
 static void main_window_load(Window *window) {
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  
-  //Make sure the time is displayed from the start
-  update_time();
+  //text_layer_set_text(s_time_layer, "00:00"); this isn't needed since we call update_time()
   
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -42,11 +37,18 @@ static void main_window_load(Window *window) {
   
   //Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  //Make sure the time is displayed from the start
+  update_time();
 }
 
 static void main_window_unload(Window *window) {
   //Destroy TextLayer
   text_layer_destroy(s_time_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void init() {
